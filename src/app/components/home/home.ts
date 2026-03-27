8ddb42f4d172d55707bd920ca6ac76f4e8f73b3b
Carlos Ramos <carlosandresramos803@outlook.com>
2026-03-27 18:39:52 -0500

    Update

--- src/app/components/home/home.ts
+++ src/app/components/home/home.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
 import { MATERIAL_MODULES } from '../../shared/material';
+import { ImageDialog } from '../image-dialog/image-dialog';
 
 @Component({
   selector: 'app-home',
@@ -8,5 +10,18 @@ import { MATERIAL_MODULES } from '../../shared/material';
   styleUrl: './home.scss',
 })
 export class Home {
+
+  constructor(private dialog: MatDialog) {}
+
+  openImage(image: string) {
+    this.dialog.open(ImageDialog, {
+      data: { image },
+      panelClass: 'custom-dialog',
+      maxWidth: '95vw',
+      maxHeight: '95vh',
+      width: 'auto',
+      height: 'auto'
+    });
+  }
 
 }
